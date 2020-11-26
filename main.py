abcbb679eadb8c31e41676f68ff27f2c1d9d1220
Ankit Kaushal <kaushalankit101010@gmail.com>
2020-11-26 20:05:09 +0530

    initial

---
+++ main.py
@@ -0,0 +1,22 @@
+from __future__ import annotations
+
+from typing import Generator
+
+import requests
+from bs4 import BeautifulSoup
+
+url = "https://www.indeed.co.in/jobs?q=mobile+app+development&l="
+
+
+def fetch_jobs(location: str = "mumbai") -> Generator[tuple[str, str], None, None]:
+    soup = BeautifulSoup(requests.get(url + location).content, "html.parser")
+    for job in soup.find_all("div", attrs={"data-tn-component": "organicJob"}):
+        job_title = job.find("a", attrs={"data-tn-element": "jobTitle"}).text.strip()
+        company_name = job.find("span", {"class": "company"}).text.strip()
+        yield job_title, company_name
+
+
+if __name__ == "__main__":
+    place = ''
+    for i, job in enumerate(fetch_jobs(place), 1):
+        print(f"Job {i:>2} is {job[0]} at {job[1]}")
